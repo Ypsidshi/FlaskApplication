8cf8fcc247915043b1a257acee782e11a828c054
Artyom <92441388+Ypsidshi@users.noreply.github.com>
2024-06-07 12:45:18 +0300

    Add files via upload
    Added using of jinja2 and developed main file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ["Новости", "О нас", "Галерея"]
+
 @app.route("/")
 def index():
-    return "index"
+    return render_template('index.html', menu=menu)
 
 @app.route("/news")
 def news():
-    return "<h1> Новости </h1>"
+    return render_template('news.html', menu=menu)
 if __name__ == '__main__':
     app.run(debug=True)
